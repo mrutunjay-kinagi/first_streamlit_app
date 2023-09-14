1bf094f8492ec5fafb7dfa822e0db78ec5676877
mrutunjay-kinagi <mrutunjay.kinagi@gmail.com>
2023-09-14 23:32:14 +0530

    Update streamlit_app.py
    Try/Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.stop()
 
 streamlit.header("The Fruit Load list contains: ")
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return  my_cur.fetchall()
 
@@ -54,7 +54,7 @@ if streamlit.button('Get Fruit load list :')
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
-    with with my_cnx.cursor() as my_cur
+    with with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding"+new_fruit
 
